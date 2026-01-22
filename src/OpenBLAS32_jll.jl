cf8228567289b5bba672ea926c2ff77e523b4461
jlbuild <juliabuildbot@gmail.com>
2026-01-22 15:20:55 +0000

    OpenBLAS32_jll build 0.3.30+0

--- src/OpenBLAS32_jll.jl
+++ src/OpenBLAS32_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("OpenBLAS32")
-JLLWrappers.@generate_main_file("OpenBLAS32", UUID("656ef2d0-ae68-5445-9ca0-591084a874a2"))
+JLLWrappers.@generate_main_file("OpenBLAS32", Base.UUID("656ef2d0-ae68-5445-9ca0-591084a874a2"))
 end  # module OpenBLAS32_jll
